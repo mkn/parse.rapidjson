fa65d693227711d62b5a0edf7bc51384ab5b5f75
dekken <philip.deegan@gmail.com>
2017-02-12 19:38:24 +0000

    more

--- mkn.sh
+++ mkn.sh
@@ -1,5 +1,6 @@
 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 URL="https://github.com/miloyip/rapidjson"
-rm -rf r
-git clone --depth 1 $URL -b master r --recursive
+rm -rf $CWD/r
+git clone --depth 1 $URL -b master $CWD/r --recursive
